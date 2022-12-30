f8d785dce77051c8741dc28ee8b30c4c7954ba07
SalihYalcin47 <99169193+SalihYalcin47@users.noreply.github.com>
2022-12-30 20:39:08 +0300

    V1

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /* 30 Aralik 2023 saat 22.00
+Problem Tanımı :
+Basit 4 işlem yapan bir hesap makinesi kodlayınız....
+Kullanicidan yapacagi islemi islem sembolu ile secmesini saglayinız.
+Kullanicidan iki sayi girmesini isteyiniz.
+Girilen iki sayi ve secilen isleme gore dogru sonucu ekrana yazdirınız.
+
+***cikan sonucla yeni bi islem yaptirmayi sectirip isleme devam ettirin
+***hesap makinesi ve islemler methodla calissin.
+*/
+
+        while(true)
+        {
+        Scanner scan=new Scanner(System.in);
+            System.out.println();
+        System.out.println("################  HESAP MAKİNESİ ################");
+        System.out.print("Lütfen İlk Sayınızı Giriniz:");
+        int sayi1 = scan.nextInt();
+        System.out.print("Lütfen Yapmak İstediğiniz işlemin işaretini giriniz (+,-,*,/) :");
+        char islem = scan.next().charAt(0);
+        System.out.print("Lütfen ikinci Sayınızı Giriniz:");
+        int sayi2 = scan.nextInt();
+
+        switch (islem)
+        {
+            case '+':
+                System.out.println(sayi1+sayi2);
+                System.out.println("###############################################");break;
+            case '-':
+                System.out.println(sayi1-sayi2);
+            System.out.println("###############################################");break;
+            case '*':
+                System.out.println(sayi1*sayi2);
+                System.out.println("###############################################");break;
+            case '/':
+                System.out.println(sayi1/sayi2);
+                System.out.println("###############################################");break;
+            default:
+                System.out.println("Yanlış İşlem Lütfen Tekrar Deneyiniz"); break;
+        }
+    }
+
+    }
+}
